71ecae3eaca167c422c98f3b2da7f338a35ad17e
R4iju <github@mozdom.mozmail.com>
2024-03-20 16:57:41 +0900

    zod readonly?

--- apps/excalifork/src/server/api/routers/drawings-schema.ts
+++ apps/excalifork/src/server/api/routers/drawings-schema.ts
@@ -33,7 +33,7 @@ export const ElementSchema = z.object({
   frameId: z.string().nullable(),
   roundness: z.any().optional(),
   // boundElements: z.array(BoundElement).or(z.array(BoundElement).readonly()).nullable(),
-  points: z.array(z.any()).optional(),
+  points: z.any(),
   boundElements: z.any(),
   updated: z.number(),
   link: z.string().nullable(),
