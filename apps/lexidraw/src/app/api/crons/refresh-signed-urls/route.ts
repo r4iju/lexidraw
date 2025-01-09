b98834895c5a05d93b45573c83d64632bc23f8da
r4iju <5772718+r4iju@users.noreply.github.com>
2025-01-09 02:57:07 +0000

    fix cronjob

--- apps/lexidraw/src/app/api/crons/refresh-signed-urls/route.ts
+++ apps/lexidraw/src/app/api/crons/refresh-signed-urls/route.ts
@@ -2,13 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import { s3 } from "~/server/s3";
 import { GetObjectCommand } from "@aws-sdk/client-s3";
 import env from "@packages/env";
-import { asc, drizzle, eq, lte, schema } from "@packages/drizzle";
+import { and, asc, drizzle, eq, lte, schema } from "@packages/drizzle";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import type { ServerRuntime } from "next";
 import { canRunCron } from "../cron-middleware";
 
 export const maxDuration = 120; // 2 minutes
 export const runtime: ServerRuntime = "edge";
+export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest) {
   console.log("#".repeat(20), " Cron job started ", "#".repeat(20));
@@ -31,9 +32,6 @@ export async function GET(request: NextRequest) {
     const uploadedImages = await drizzle
       .select({
         id: schema.uploadedImages.id,
-        kind: schema.uploadedImages.kind,
-        signedDownloadUrl: schema.uploadedImages.signedDownloadUrl,
-        fileName: schema.uploadedImages.fileName,
         entityId: schema.uploadedImages.entityId,
         updatedAt: schema.uploadedImages.updatedAt,
       })
@@ -52,113 +50,110 @@ export async function GET(request: NextRequest) {
     console.log("Found images in table:", uploadedImages.length);
 
     // Process one entity at a time to avoid holding large data in memory
-    let currentEntityId: string | null = null;
-    let currentEntity = null;
-    let currentEntityDarkThumbnail = null;
-    let currentEntityLightThumbnail = null;
 
-    for (const image of uploadedImages) {
-      // If entityId changes, fetch the new entity
-      if (image.entityId !== currentEntityId) {
-        if (currentEntity && currentEntityId) {
-          // Update the previous entity if any changes were made
-          await drizzle
-            .update(schema.entities)
-            .set({
-              elements: currentEntity.elements,
-              updatedAt: new Date(),
-            })
-            .where(eq(schema.entities.id, currentEntityId))
-            .execute();
-          console.log(`Updated entity ${currentEntityId}`);
-        }
+    for (const currentEntityId of [
+      ...new Set(uploadedImages.map((image) => image.entityId)),
+    ]) {
+      const currentEntity = await drizzle
+        .select({
+          id: schema.entities.id,
+          userId: schema.entities.userId,
+          publicAccess: schema.entities.publicAccess,
+          elements: schema.entities.elements,
+          screenShotDark: schema.entities.screenShotDark,
+          screenShotLight: schema.entities.screenShotLight,
+        })
+        .from(schema.entities)
+        .where(eq(schema.entities.id, currentEntityId))
+        .then((rows) => rows[0]);
 
-        // Fetch the new entity
-        currentEntityId = image.entityId;
-        currentEntity = await drizzle
-          .select({
-            id: schema.entities.id,
-            userId: schema.entities.userId,
-            publicAccess: schema.entities.publicAccess,
-            elements: schema.entities.elements,
-          })
-          .from(schema.entities)
-          .where(eq(schema.entities.id, currentEntityId))
+      if (!currentEntity) {
+        console.warn(`Entity not found for ID: ${currentEntityId}`);
+        continue;
+      }
+
+      let updatedElements = currentEntity.elements;
+      let screenShotDark = currentEntity.screenShotDark;
+      let screenShotLight = currentEntity.screenShotLight;
+
+      for (const _image of uploadedImages) {
+        const image = await drizzle
+          .select()
+          .from(schema.uploadedImages)
+          .where(
+            and(
+              eq(schema.uploadedImages.id, _image.id),
+              eq(schema.uploadedImages.entityId, currentEntityId),
+            ),
+          )
           .then((rows) => rows[0]);
 
-        if (!currentEntity) {
-          console.warn(`Entity not found for ID: ${currentEntityId}`);
-          currentEntityId = null;
-          currentEntity = null;
+        if (!image) {
+          console.warn(`Image not found for ID: ${_image.id}`);
           continue;
         }
 
-        console.log(`Processing entity: ${currentEntityId}`);
-      }
+        // Generate a new signed download URL
+        const downloadCommand = new GetObjectCommand({
+          Bucket: env.SUPABASE_S3_BUCKET,
+          Key: image.fileName,
+        });
+        const signedDownloadUrl = await getSignedUrl(s3, downloadCommand, {
+          expiresIn: 7 * 24 * 60 * 60,
+        });
 
-      if (!currentEntity) continue;
+        // Update the image record
+        await drizzle
+          .update(schema.uploadedImages)
+          .set({
+            signedDownloadUrl,
+            updatedAt: new Date(),
+          })
+          .where(eq(schema.uploadedImages.id, image.id))
+          .execute();
 
-      // Generate a new signed download URL
-      const downloadCommand = new GetObjectCommand({
-        Bucket: env.SUPABASE_S3_BUCKET,
-        Key: image.fileName,
-      });
-      const signedDownloadUrl = await getSignedUrl(s3, downloadCommand, {
-        expiresIn: 7 * 24 * 60 * 60,
-      });
-
-      // Update the image record
-      await drizzle
-        .update(schema.uploadedImages)
-        .set({
-          signedDownloadUrl,
-          updatedAt: new Date(),
-        })
-        .where(eq(schema.uploadedImages.id, image.id))
-        .execute();
-
-      console.log(
-        `Updated download URL for image ${image.id}. Old URL: ${image.signedDownloadUrl}, New URL: ${signedDownloadUrl}`,
-      );
-
-      // Replace URLs in the entity's elements
-      if (image.kind === "attachment") {
-        currentEntity.elements = currentEntity.elements.replaceAll(
-          image.signedDownloadUrl,
-          signedDownloadUrl,
+        console.log(
+          `Updated download URL for image ${image.id}. Old URL: ${image.signedDownloadUrl}, New URL: ${signedDownloadUrl}`,
         );
-        updatedCount += 1;
-      }
-      if (image.kind === "thumbnail") {
-        if (image.fileName.includes("dark")) {
-          currentEntityDarkThumbnail = signedDownloadUrl;
-        } else {
-          currentEntityLightThumbnail = signedDownloadUrl;
-        }
-        updatedCount += 1;
-      }
-    }
 
-    // Final update for the last entity
-    if (currentEntity && currentEntityId) {
-      await drizzle
-        .update(schema.entities)
-        .set({
-          // only update the parts that have changed
-          ...(currentEntity.elements !== currentEntity.elements
-            ? { elements: currentEntity.elements }
-            : {}),
-          ...(currentEntityDarkThumbnail
-            ? { darkThumbnail: currentEntityDarkThumbnail }
-            : {}),
-          ...(currentEntityLightThumbnail
-            ? { lightThumbnail: currentEntityLightThumbnail }
-            : {}),
-          updatedAt: new Date(),
-        })
-        .where(eq(schema.entities.id, currentEntityId))
-        .execute();
-      console.log(`Updated entity ${currentEntityId}`);
+        // Replace URLs in the entity's elements
+        if (image.kind === "attachment") {
+          updatedElements = updatedElements.replaceAll(
+            image.signedDownloadUrl,
+            signedDownloadUrl,
+          );
+          updatedCount += 1;
+        }
+        if (image.kind === "thumbnail") {
+          if (image.fileName.includes("dark")) {
+            screenShotDark = signedDownloadUrl;
+          } else {
+            screenShotLight = signedDownloadUrl;
+          }
+          updatedCount += 1;
+        }
+      }
+
+      // Final update for the last entity
+      if (currentEntity && currentEntityId) {
+        await drizzle
+          .update(schema.entities)
+          .set({
+            ...(updatedElements !== currentEntity.elements
+              ? { elements: updatedElements }
+              : {}),
+            ...(screenShotDark !== currentEntity.screenShotDark
+              ? { screenShotDark }
+              : {}),
+            ...(screenShotLight !== currentEntity.screenShotLight
+              ? { screenShotLight }
+              : {}),
+            updatedAt: new Date(),
+          })
+          .where(eq(schema.entities.id, currentEntityId))
+          .execute();
+        console.log(`Updated entity ${currentEntityId}`);
+      }
     }
 
     console.log("#".repeat(20), " Cron job finished ", "#".repeat(20));
