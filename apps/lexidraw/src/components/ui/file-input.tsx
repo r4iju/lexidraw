ebf785409ba7edc48cb2818fe39247f9c5e522a1
r4iju <5772718+r4iju@users.noreply.github.com>
2025-04-28 00:19:22 +0000

    streamline inline image style

--- apps/lexidraw/src/components/ui/file-input.tsx
+++ apps/lexidraw/src/components/ui/file-input.tsx
@@ -1,7 +1,10 @@
+import { cn } from "~/lib/utils";
+import { Label } from "./label";
+
 type Props = Readonly<{
-  "data-test-id"?: string;
   accept?: string;
-  label: string;
+  label?: string;
+  className?: string;
   onChange: (files: FileList | null) => void;
 }>;
 
@@ -9,17 +12,18 @@ export default function FileInput({
   accept,
   label,
   onChange,
-  "data-test-id": dataTestId,
+  className,
 }: Props): React.JSX.Element {
   return (
-    <div className="flex flex-row items-center mb-2">
-      <label className="flex-1 text-muted-foreground">{label}</label>
+    <div className={cn("flex flex-col gap-2", className)}>
+      {label && <Label className="flex-1 whitespace-nowrap">{label}</Label>}
       <input
         type="file"
         accept={accept}
-        className="flex-2 border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+        className={cn(
+          "flex-2 h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        )}
         onChange={(e) => onChange(e.target.files)}
-        data-test-id={dataTestId}
       />
     </div>
   );
