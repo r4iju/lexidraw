82d73e7b63f0e89f90a8cc05c583cbfa6b018d84
r4iju <5772718+r4iju@users.noreply.github.com>
2025-05-25 23:51:18 +0000

    add debug

--- apps/lexidraw/src/app/documents/[documentId]/plugins/LlmChatPlugin/ui/sidebar.tsx
+++ apps/lexidraw/src/app/documents/[documentId]/plugins/LlmChatPlugin/ui/sidebar.tsx
@@ -3,16 +3,26 @@ import { ModeTabs } from "./mode-tabs";
 import { MessageList } from "./message-list";
 import { MessageInput } from "./message-input";
 import { Reset } from "./reset";
+import { useChatState } from "../llm-chat-context";
+import { DebugPanel } from "./debug-panel";
 
 export const Sidebar: React.FC = () => {
+  const { mode } = useChatState();
+
   return (
     <div className="flex flex-col flex-1 h-full overflow-hidden">
       <div className="p-2 flex justify-between gap-2">
         <ModeTabs />
         <Reset />
       </div>
-      <MessageList />
-      <MessageInput />
+      {mode === "debug" ? (
+        <DebugPanel />
+      ) : (
+        <>
+          <MessageList />
+          <MessageInput />
+        </>
+      )}
     </div>
   );
 };
