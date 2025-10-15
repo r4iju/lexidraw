69f07c5951f6a1f8c35a7a151593e860beb348eb
Your Name <eman.franzen@gmail.com>
2025-10-15 22:54:38 +0000

    move to biome

--- apps/lexidraw/prettier.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindStylesheet: "./src/styles/globals.css",
-};
-
-export default config;
