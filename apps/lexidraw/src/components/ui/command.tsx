f5a3a2d275c69c6ebf14392e5091689baee12b97
r4iju <5772718+r4iju@users.noreply.github.com>
2025-12-14 20:15:10 +0900

    refactor: Clean up type imports and enhance CommandDialog component
    This commit includes the following changes:
    - Removed unused type import from dnd-context.tsx.
    - Updated CommandDialog component to use a more specific type for children, improving type safety.
    - Adjusted class names in Command component for better readability and consistency.

--- apps/lexidraw/src/components/ui/command.tsx
+++ apps/lexidraw/src/components/ui/command.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import type * as React from "react";
 import type { DialogProps } from "@radix-ui/react-dialog";
 import { Command as CommandPrimitive } from "cmdk";
 import { Search } from "lucide-react";
@@ -24,11 +23,18 @@ Command.displayName = CommandPrimitive.displayName;
 
 type CommandDialogProps = DialogProps;
 
-const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
+type CommandDialogPropsWithChildren = Omit<CommandDialogProps, "children"> & {
+  children?: React.ReactNode;
+};
+
+const CommandDialog = ({
+  children,
+  ...props
+}: CommandDialogPropsWithChildren) => {
   return (
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0 shadow-lg">
-        <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
+        <Command className="**:[[cmdk-group-heading]]:px-2 **:[[cmdk-group-heading]]:font-medium **:[[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 **:[[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 **:[[cmdk-input]]:h-12 **:[[cmdk-item]]:px-2 **:[[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
       </DialogContent>
@@ -92,7 +98,7 @@ type CommandGroupProps = React.ComponentPropsWithRef<
 const CommandGroup = ({ className, ...props }: CommandGroupProps) => (
   <CommandPrimitive.Group
     className={cn(
-      "overflow-hidden p-1 text-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground",
+      "overflow-hidden p-1 text-foreground **:[[cmdk-group-heading]]:px-2 **:[[cmdk-group-heading]]:py-1.5 **:[[cmdk-group-heading]]:text-xss **:[[cmdk-group-heading]]:font-medium**:[[cmdk-group-heading]]:text-muted-foregroundd",
       className,
     )}
     {...props}
